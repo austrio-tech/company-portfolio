38aabebf089aa7e23f32e433744d883512f8ede8
Muhammad Athar <58026305+Muhammad-Athar@users.noreply.github.com>
2024-07-19 23:40:53 +0500

    layout chnaged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="w-screen h-screen bg-green-600">
-    home
-    </div>
-  );
+    return <div className="w-screen h-screen bg-green-600">home</div>;
 }
